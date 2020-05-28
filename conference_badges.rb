f2c37dfea8a9a0a178b02af9bfc9ac0238b944e6
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-28 23:37:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,21 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(names)
+  names.map do |name|
+    "Hello, my name is #{name}.
+  end
+end
+
+
+
+# [
+#                 "Hello, my name is Edsger.",
+#                 "Hello, my name is Ada.",
+#                 "Hello, my name is Charles.",
+#                 "Hello, my name is Alan.",
+#                 "Hello, my name is Grace.",
+#                 "Hello, my name is Linus.",
+#                 "Hello, my name is Matz."
+#              ]
