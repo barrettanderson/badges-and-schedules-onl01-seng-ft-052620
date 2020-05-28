8d6d6fb9d6c3015e381d3a324fb2691dc3aa8868
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-28 21:38:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,5 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.collect do |name|
-    return "Hello, my name is #{name}."
-  end
+  attendees.collect {|attendees| puts "Hello, my name is #{attendees}."}
 end
